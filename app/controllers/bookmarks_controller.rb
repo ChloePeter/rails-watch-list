74f8da6c43ea041fd566cab0e7190521984087ab
Chloe Peter <chloe1peter@outlook.fr>
2022-02-17 16:51:50 +0100

    Challenge 2 day 28

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,18 @@
+class BookmarksController < ApplicationController def new
+
+  @bookmark = Bookmark.new
+end
+
+def create
+  @bookmark = Bookmark.new(bookmark_params)
+  if @bookmark.save
+    redirect_to bookmark_path(@bookmark)
+  else
+    render "new"
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to bookmarks_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+resources :lists, only: %i[index show new create]
+
+get "/lists/new", to: "bookmark#new" as: :bookmark
+post "/lists", to: "bookmark#create" as: :bookmark
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
