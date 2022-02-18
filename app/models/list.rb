70768c155e12de1f06a34516b88964f7a49f416e
Chloe Peter <chloe1peter@outlook.fr>
2022-02-18 11:26:33 +0100

    save

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
-  has_many :bookmarks
-  has_many :movies, through: :bookmarks, dependent: :destroy
-  # validates_associated :bookmarks through bookmarks
-  validates :name, presence: true, uniqueness: true
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+  has_many :reviews, dependent: :destroy
+  validates :name, uniqueness: true, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 
-get "/lists/new", to: "bookmark#new" as: :bookmark
-post "/lists", to: "bookmark#create" as: :bookmark
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
